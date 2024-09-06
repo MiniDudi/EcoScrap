bf220765ea027a7c2992b2e8152285de4d16ef94
jvJUCA <jucafranzoni1@gmail.com>
2024-09-06 16:27:07 -0300

    feat: firebase configuration added

---
+++ eco-scrap/src/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz9B7GTWgIzUqAL1CX6eBFdQg1AyeHrNQ",
+
+  authDomain: "ecoscrap-ca403.firebaseapp.com",
+
+  projectId: "ecoscrap-ca403",
+
+  storageBucket: "ecoscrap-ca403.appspot.com",
+
+  messagingSenderId: "315575987355",
+
+  appId: "1:315575987355:web:844abae94f36aa5824ea3e",
+
+  measurementId: "G-VC1675MWGJ",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { analytics, storage, db, auth };
