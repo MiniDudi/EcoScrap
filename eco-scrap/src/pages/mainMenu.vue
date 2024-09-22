53b1d991a5402b9109fba359ba83a6602a28a5b1
jvJUCA <jucafranzoni1@gmail.com>
2024-09-22 17:35:38 -0300

    feat: improved style and added tabs

--- eco-scrap/src/pages/mainMenu.vue
+++ eco-scrap/src/pages/mainMenu.vue
@@ -1,69 +1,115 @@
 <template>
     <!-- Main -->
     <!-- NavBar -->
-    <v-container fluid style="background-color: #0a1015; width: 95vw; height: 16vh" class="ma-10">
+    <v-container fluid style="background-color: #0a1015; height: 14vh">
         <v-row no-gutters>
-
             <!-- Logo -->
-            <v-col cols="4">
-                <!-- PROBLEMA COM TAMANHO DA LOGO ----- PROCURAR CSS ----- SEM MARGIN NEGATIVA -->
-                <v-img src="../../public/logo.svg" contain height="250" width="250" style="margin-top: -50px;"
-                    class="logo-overlay"></v-img>
+            <v-col cols="4" class="d-flex align-center">
+                <v-img src="../../public/logo.svg" cover max-height="100" max-width="250"></v-img>
             </v-col>
 
             <!-- Texto -->
-            <v-col cols="4" class="mt-4">
+            <v-col cols="4" class="d-flex flex-column justify-center mt-4">
                 <p class="primary-text" style="color: white; font-family: Khand; font-size: 25px; letter-spacing: 3px;">
                     PLATAFORMA DE COTAÇÃO
                 </p>
                 <p class="development-text"
-                    style="color: #5fd136; font-family: Khand; font-size: 40px; font-weight:600;">DESENVOLVIMENTO</p>
+                    style="color: #5fd136; font-family: Khand; font-size: 40px; font-weight:600;">
+                    DESENVOLVIMENTO
+                </p>
             </v-col>
 
             <!-- Site and Login -->
-            <v-col cols="4">
-
+            <v-col cols="4" class="d-flex flex-column justify-center align-end">
                 <!-- Site -->
-                <v-row no-gutters style="position: fixed; top: 10; right: 10;">
-                    <v-avatar class="mr-1" color="green" size="20" />
-                    <p class="site-text" style="color: white; font-family: Kdam Thmor Pro; f">www.ecoscrap.com.br</p>
+                <v-row no-gutters class="mb-2">
+                    <v-avatar class="mr-2" color="green" size="15" />
+                    <p class="site-text" style="color: white; font-family: Kdam Thmor Pro;">
+                        www.ecoscrap.com.br
+                    </p>
                 </v-row>
 
                 <!-- Login -->
-                <v-row no-gutters>
-                    <v-col align-self="end">
-                        <p class="login-text">OLÁ {{ usuario }}, LOGOUT</p>
-
-                    </v-col>
+                <v-row no-gutters align="end">
+                    <span class="login-text">OLÁ {{ usuario }}</span>
                 </v-row>
-
             </v-col>
         </v-row>
-
-        <!-- Utils Container -->
-        <v-container style="width: 96vw; height: 6vh; color: #d9d9d9; border-radius: 50px50 50px 50px 50px;">
-            <p>aaaaa</p>
-        </v-container>
-
     </v-container>
 
+    <!-- Menu -->
+    <v-container fluid class="pa-0">
+        <v-col cols="12" class="pa-0">
+            <v-card flat color="white" width="100%">
+                <v-card-text class="pa-2">
+                    <div class="d-flex justify-space-around">
+                        <span @click="selectTab(0)" :class="['tab-text', { 'font-weight-bold': selectedTab === 0 }]"
+                            class="cursor-pointer">
+                            Cotação
+                        </span>
+                        <v-divider vertical thickness="3"></v-divider>
+                        <span @click="selectTab(1)" :class="['tab-text', { 'font-weight-bold': selectedTab === 1 }]"
+                            class="cursor-pointer">
+                            Material
+                        </span>
+                        <v-divider vertical thickness="3"></v-divider>
+                        <span @click="selectTab(2)" :class="['tab-text', { 'font-weight-bold': selectedTab === 2 }]"
+                            class="cursor-pointer">
+                            Denúncias
+                        </span>
+                    </div>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-container>
 
-    <!-- Notícias -->
-    <v-container class="news-container mx-10" fluid>
+    <!-- News -->
+    <v-container fluid class="pt-3">
         <v-row style="background: #0A1015;">
-            <v-col cols="8">
-                <div>
-                    <span style="color:white; font-weight: 700;">RELATORIO DE PRECO</span>
-                </div>
+            <v-col cols="7">
+                <span class="ml-5" style="color:white; font-weight: 700;">RELATORIO DE PRECO</span>
             </v-col>
-            <v-col cols="4">
-                <div class="pl-4"
-                    style="background-color: #f36f6f; border-top-left-radius: 50px; border-bottom-left-radius: 50px;">
-                    <span style="color:white;">ULTIMA ATUALIZAÇÃO: {{ lastUpdate.toString() }}</span>
-                </div>
+            <v-col cols="5" class="pa-0 d-flex align-center justify-center"
+                style="background-color: #F78386; clip-path: polygon(15% 0%, 100% 0%, 100% 100%, 15% 100%, 0% 50%);">
+                <span style="color:white;">ULTIMA ATUALIZAÇÃO: {{ lastUpdate.toString() }}</span>
             </v-col>
         </v-row>
+    </v-container>
 
+    <!-- Cards -->
+    <v-container fluid class="py-0">
+        <v-row class="mt-3">
+            <v-col cols="4">
+                <v-sheet class="d-flex flex-column justify-space-between material-card cursor-pointer"
+                    style="background-color: #F78386; height: 150px;">
+                    <v-img src="../assets/garrafa.svg" contain min-height="100" min-width="80" max-height="80"
+                        max-width="200" class="mt-2"></v-img>
+                    <div class="pa-2" style="background-color: #E5686B; width: 100%;">
+                        <span style="color: white; font-weight: 700;">PLÁSTICO</span>
+                    </div>
+                </v-sheet>
+            </v-col>
+            <v-col cols="4">
+                <v-sheet class="d-flex flex-column justify-space-between material-card cursor-pointer"
+                    style="background-color: #F7E386; height: 150px;">
+                    <v-img src="../assets/lata.svg" contain min-height="80" min-width="80" max-height="80"
+                        max-width="200" class="mt-4"></v-img>
+                    <div class="pa-2" style="background-color: #E5D16B; width: 100%;">
+                        <span style="color: white; font-weight: 700;">ALUMÍNIO</span>
+                    </div>
+                </v-sheet>
+            </v-col>
+            <v-col cols="4">
+                <v-sheet class="d-flex flex-column justify-space-between material-card cursor-pointer"
+                    style="background-color: #5FD136; height: 150px;">
+                    <v-img src="../assets/ferro.svg" contain min-height="80" min-width="80" max-height="80"
+                        max-width="200" class="mt-4"></v-img>
+                    <div class="pa-2" style="background-color: #4DB02B; width: 100%;">
+                        <span style="color: white; font-weight: 700;">FERRO</span>
+                    </div>
+                </v-sheet>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -74,55 +120,52 @@ export default {
             usuario: "dudu e juca",
             lastUpdate: Date.now(),
             materials: [
-                {
-                    name: "FERRO",
-                    price: 0,
-                    type: "Metal"
-                },
-                {
-                    name: "PLÁSTICO",
-                    price: 0,
-                    type: "Plastico"
-                },
-                {
-                    name: "METAL",
-                    price: 0,
-                    type: "Metal"
-                },
-                {
-                    name: "ALUMÍNIO",
-                    price: 0,
-                    type: "Metal"
-                },
-                {
-                    name: "PAPELÃO",
-                    price: 0,
-                    type: "Papel"
-                },
-                {
-                    name: "LATÃO",
-                    price: 0,
-                    type: "Metal"
-                },
-                {
-                    name: "COBRE",
-                    price: 0,
-                    type: "Metal"
-                }
-            ]
+                { name: "FERRO", price: 0, type: "Metal" },
+                { name: "PLÁSTICO", price: 0, type: "Plastico" },
+                { name: "METAL", price: 0, type: "Metal" },
+                { name: "ALUMÍNIO", price: 0, type: "Metal" },
+                { name: "PAPELÃO", price: 0, type: "Papel" },
+                { name: "LATÃO", price: 0, type: "Metal" },
+                { name: "COBRE", price: 0, type: "Metal" }
+            ],
+            selectedTab: 0
         }
     },
+    methods: {
+        selectTab(index) {
+            this.selectedTab = index;
+        }
+    }
 }
 </script>
 
 <style scoped>
 .login-text {
     color: white;
-    font-family: Khula;
-    font-size: 10px;
+    font-size: 20px;
 }
 
-.news-container {
-    width: 95vw;
+v-application {
+    background-color: #818181;
+}
+
+.tab-text {
+    color: rgb(48, 48, 48);
+    font-size: 24px;
+    margin: 0 20px;
+    cursor: pointer;
+    transition: font-weight 0.3s ease;
+}
+
+.tab-text.font-weight-bold {
+    font-weight: 700;
+}
+
+.material-card {
+    transition: transform 0.3s ease;
+}
+
+.material-card:hover {
+    transform: scale(1.05);
 }
 </style>
